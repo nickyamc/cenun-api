7a9b6d421ed4152135b3899180aa1774ff549385
Nick Ángel Macedo Córdova <nmacedocordova@gmail.com>
2023-11-14 20:28:15 -0500

    test deploy
    test deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,61 +1,69 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { LabModule } from './lab/lab.module';
-import { EventModule } from './event/event.module';
-import { UserModule } from './user/user.module';
-import { VisitorModule } from './visitor/visitor.module';
-import { AuthModule } from './auth/auth.module';
-import { AttendanceModule } from './attendance/attendance.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './user/user.entity';
-import { Visitor } from './visitor/visitor.entity';
-import { Evento } from './event/event.entity';
-import { Lab } from './lab/lab.entity';
-import { Attendance } from './attendance/attendance.entity';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {LabModule} from './lab/lab.module';
+import {EventModule} from './event/event.module';
+import {UserModule} from './user/user.module';
+import {VisitorModule} from './visitor/visitor.module';
+import {AuthModule} from './auth/auth.module';
+import {AttendanceModule} from './attendance/attendance.module';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {User} from './user/user.entity';
+import {Visitor} from './visitor/visitor.entity';
+import {Evento} from './event/event.entity';
+import {Lab} from './lab/lab.entity';
+import {Attendance} from './attendance/attendance.entity';
+import {ConfigModule, ConfigService} from '@nestjs/config';
 import configuration from './config/configuration';
-import { cwd, env } from 'process';
-import { SessionModule } from './session/session.module';
+import {cwd, env} from 'process';
+import {SessionModule} from './session/session.module';
 import {SessionEntity} from "./session/session.entity";
+import {DatabaseType} from "typeorm/driver/types/DatabaseType";
 
 @Module({
-	imports: [
-		ConfigModule.forRoot({
-			envFilePath: `${cwd()}/.env.${env.NODE_ENV}`,
-			load: [configuration],
-			isGlobal: true,
-		}),
-		TypeOrmModule.forRootAsync({
-			imports: [ConfigModule],
-			useFactory: async (configureService: ConfigService) => ({
-				type: 'postgres',
-				host: configureService.get<string>('database.host'),
-				port: configureService.get<number>('database.port'),
-				username: configureService.get<string>('database.username'),
-				password: configureService.get<string>('database.password'),
-				database: configureService.get<string>('database.database'),
-				entities: [User, Visitor, Evento, Lab, Attendance, SessionEntity],
-				uuidExtension: 'uuid-ossp', // o 'pgcrypto' o 'uuid-ossp'
-				synchronize: true,
-				logging: true,
-			}),
-			inject: [ConfigService],
-		}),
-		LabModule,
-		EventModule,
-		UserModule,
-		VisitorModule,
-		AuthModule,
-		AttendanceModule,
-		SessionModule,
-	],
-	controllers: [AppController],
-	providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: `${cwd()}/.env`,//.${env.NODE_ENV}
+            load: [configuration],
+            isGlobal: true,
+        }),
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: async (configService: ConfigService) => ({
+                type: configService.get<DatabaseType>('database.type') as any,
+                host: configService.get<string>(`database.host`),
+                port: configService.get<number>(`${configService.get<string>('database.type')}.port`),
+                username: configService.get<string>(`${configService.get<string>('database.type')}.username`),
+                password: configService.get<string>(`${configService.get<string>('database.type')}.password`),
+                database: configService.get<string>(`${configService.get<string>('database.type')}.database`),
+                entities: [User, Visitor, Evento, Lab, Attendance, SessionEntity],
+                logging: false,
+                //autoLoadEntities: true,
+                uuidExtension: 'uuid-ossp', // o 'pgcrypto' o 'uuid-ossp'
+                synchronize: true,
+                ssl: configService.get<boolean>('database.ssl'),
+                extra: {
+                    ssl: configService.get<boolean>('database.ssl')
+                        ? {rejectUnauthorized: false} : null,
+                },
+            }),
+            inject: [ConfigService],
+        }),
+        LabModule,
+        EventModule,
+        UserModule,
+        VisitorModule,
+        AuthModule,
+        AttendanceModule,
+        SessionModule,
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {
-	static port: number;
-	constructor(private readonly configService: ConfigService) {
-		AppModule.port = this.configService.get<number>('port')
-	}
+    static port: number;
+
+    constructor(private readonly configService: ConfigService) {
+        AppModule.port = this.configService.get<number>('port')
+    }
 }
--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -3,11 +3,21 @@ import {env} from "process";
 export default () => ({
     port: parseInt(env.PORT, 10) || 3000,
     database: {
-        host: env.DATABASE_HOST,
-        port: parseInt(env.DATABASE_PORT_POSTGRESQL, 10) || 3306,
-        username: env.DATABASE_USERNAME,
-        password: env.DATABASE_PASSWORD,
-        database: env.DATABASE_DATABASE,
+        type: env.DATABASE_TYPE,
+        ssl: env.DATABASE_SSL === 'true',
+        host: env.POSTGRES_HOST,
+    },
+    postgres: {
+        port: parseInt(env.POSTGRES_PORT, 10) || 5432,
+        username: env.POSTGRES_USERNAME,
+        password: env.POSTGRES_PASSWORD,
+        database: env.POSTGRES_DATABASE,
+    },
+    mariadb: {
+        port: parseInt(env.MARIADB_PORT, 10) || 3306,
+        username: env.MARIADB_USERNAME,
+        password: env.MARIADB_PASSWORD,
+        database: env.MARIADB_DATABASE,
     },
     jwt: {
         secret: env.JWT_SECRET,
